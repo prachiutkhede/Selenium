cd089bb5e75468fcaa72272b7917f9c25cc0357a
Prachi Utkhede <prachs1987@gmail.com>
2017-02-21 21:40:08 -0800

    Refactoring the code

--- Poiexample/src/main/java/com/poi/example/poiexample/PoiExample.java
+++ Poiexample/src/main/java/com/poi/example/poiexample/PoiExample.java
@@ -10,30 +10,53 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class PoiExample {
-	static FileInputStream fs = null;
-	static XSSFWorkbook wb = null;
-	static XSSFSheet sheet = null;
-	static XSSFRow row = null;
-	static XSSFCell col = null;
+    private FileInputStream fs = null;
+    private XSSFWorkbook wb = null;
+    private XSSFSheet sheet = null;
 
-	public static void getCelldata(String xlsFileName) throws IOException {
-		fs = new FileInputStream(xlsFileName);
-		wb = new XSSFWorkbook(fs);
-		sheet = wb.getSheet("register");
+    public PoiExample(String fileName) throws IOException {
+        fs = new FileInputStream(fileName);
+        wb = new XSSFWorkbook(fs);
+        sheet = wb.getSheet("register");      
+    }
+    
+    public String getCelldata(int rowIndex, int columnIndex) throws IOException {
+        // Check if the rowIndex is valid.
+        if(rowIndex < 0 || rowIndex > sheet.getLastRowNum()) return null;
+        
+        // Get the row corresponding to the row index.
+        XSSFRow row = sheet.getRow(rowIndex);
+        
+        // Check if column index is valid.
+        if(columnIndex < 0 || columnIndex > row.getLastCellNum()) return null;
 
-		for (int i = 0; i <= sheet.getLastRowNum(); i++) {
-			row = sheet.getRow(i);
-			short j = row.getLastCellNum();
-			for (int k = 0; k < j; k++) {
+        // Get the column corresponding to the row.
+        XSSFCell col = row.getCell(columnIndex);
 
-				col = row.getCell(k);
-				System.out.println(col.getStringCellValue());
-			}
-		}
-	}
+        return col.getStringCellValue();
+    }
+    
+    public void printSheet() {
+        XSSFRow row;
+        XSSFCell col;
+        
+        for (int i = 0; i <= sheet.getLastRowNum(); i++) {
+            row = sheet.getRow(i);
+            short j = row.getLastCellNum();
+            for (int k = 0; k < j; k++) {
+                col = row.getCell(k);
+                System.out.print(String.format("%15s", col.getStringCellValue()));
+            }
+            System.out.println();
+        }
+    }
 
-	public static void main(String[] args) throws IOException {
-		String xlsfileName = "/Users/prachiutkhede/Documents/Poiexcelsheet.xlsx";
-		PoiExample.getCelldata(xlsfileName);
-	}
+    public static void main(String[] args) throws IOException {
+        String xlsfileName = "/Users/prachiutkhede/git/Selenium/Poiexample/src/main/resources/Poiexcelsheet.xlsx";
+        
+        PoiExample poiExample = new PoiExample(xlsfileName);
+        poiExample.printSheet();
+        
+        System.out.println("\nRow 0 Col 0: " + poiExample.getCelldata(1, 0));
+    }
 }
